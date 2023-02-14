c35340fa91dea7ad5b4df92d788dacbaa406fd03
Osrip <jan.prosi@hotmail.com>
2023-02-14 22:02:59 +0100

    Training and validation works with primitive plotting

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import datetime
 
+import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
@@ -25,7 +26,37 @@ def check_backward(model, learning_rate, device):
     return model
 
 
-def train(model, start_date_time: datetime.datetime, device, folder_path: str, num_training_samples: int,
+def validate(model, validation_data_loader):
+    criterion = nn.CrossEntropyLoss()
+    validation_losses = []
+    for i, (input_sequence, target) in enumerate(validation_data_loader):
+        input_sequence = input_sequence.float()
+        target = T.CenterCrop(size=width_height_target)(target)
+        target = target.float()
+
+        pred = model(input_sequence)
+        loss = criterion(pred, target)
+        loss = float(loss.detach().numpy())
+        validation_losses.append(loss)
+    return np.mean(validation_losses)
+
+
+def plot_losses(losses, validation_losses, sim_name):
+    plt.plot(losses)
+    plt.title('Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig('Results/{}_loss.png'.format(sim_name), dpi=100)
+    plt.show()
+    plt.plot(validation_losses)
+    plt.title('Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig('Results/{}_validation_loss.png'.format(sim_name), dpi=100)
+    plt.show()
+
+
+def train(model, train_start_date_time: datetime.datetime, device, folder_path: str, num_training_samples: int, num_validation_samples,
           minutes_per_iteration: int, width_height: int, learning_rate: int, num_epochs: int,
           num_input_time_steps: int, num_channels_one_hot_output, width_height_target, batch_size):
     accuracies = []
@@ -37,14 +68,23 @@ def train(model, start_date_time: datetime.datetime, device, folder_path: str, n
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     max_accuracy = 0
     # TODO: Enable saving to pickle at some point
-    data_sequence = load_data_sequence(start_date_time, folder_path, future_iterations_from_start=num_training_samples,
+    data_sequence = load_data_sequence(train_start_date_time, folder_path, future_iterations_from_start=num_training_samples+num_validation_samples,
                                        width_height=width_height, minutes_per_iteration=minutes_per_iteration)
-    data_set = PrecipitationDataset(data_sequence, num_pictures_loaded, num_channels_one_hot_output)
-    data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=True, drop_last=True)
+    train_data_sequence = data_sequence[0:num_training_samples, :, :]
+    validation_data_sequence = data_sequence[num_training_samples:, :, :]
+
+    train_data_set = PrecipitationDataset(train_data_sequence, num_pictures_loaded, num_channels_one_hot_output)
+    train_data_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True, drop_last=True)
+
+    validation_data_set = PrecipitationDataset(validation_data_sequence, num_pictures_loaded, num_channels_one_hot_output)
+    validation_data_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True, drop_last=True)
+
     losses = []
+    validation_losses = []
     for epoch in range(num_epochs):
-        print('Epoch: {}'.format(epoch))
-        for i, (input_sequence, target) in enumerate(data_loader):
+        inner_losses = []
+        for i, (input_sequence, target) in enumerate(train_data_loader):
+
             print('Picture: {}'.format(i))
             input_sequence = input_sequence.float()
             target = T.CenterCrop(size=width_height_target)(target)
@@ -55,6 +95,15 @@ def train(model, start_date_time: datetime.datetime, device, folder_path: str, n
             loss = criterion(pred, target)
             loss.backward()
             optimizer.step()
+
+            inner_loss = float(loss.detach().numpy())
+            inner_losses.append(inner_loss)
+        avg_inner_loss = np.mean(inner_losses)
+        losses.append(avg_inner_loss)
+        validation_loss = validate(model, validation_data_loader)
+        validation_losses.append(validation_loss)
+        print('Epoch: {} Loss: {}'.format(epoch, avg_inner_loss))
+        plot_losses(losses, validation_losses, sim_name)
 
 
 
@@ -108,7 +157,8 @@ def train(model, start_date_time: datetime.datetime, device, folder_path: str, n
 
 if __name__ == '__main__':
 
-    num_training_samples = 10 # Number of loaded pictures (first pics not used for training but only input)
+    num_training_samples = 20 # Number of loaded pictures (first pics not used for training but only input)
+    num_validation_samples = 20
     minutes_per_iteration = 5
     width_height = 256
     learning_rate = 0.0001
@@ -124,11 +174,11 @@ if __name__ == '__main__':
 
     # Throws an error on remote venv for some reason
     # optimized_model = check_backward(model, learning_rate=0.001, device='cpu')
-    start_date_time = datetime.datetime(2020, 12, 1)
+    train_start_date_time = datetime.datetime(2020, 12, 1)
     folder_path = '/media/jan/54093204402DAFBA/Jan/Programming/Butz_AG/weather_data/dwd_datensatz_bits/rv_recalc/RV_RECALC/hdf/'
     device = 'cpu'
 
-    train(model, start_date_time, device, folder_path, num_training_samples, minutes_per_iteration, width_height,
+    train(model, train_start_date_time, device, folder_path, num_training_samples, num_validation_samples, minutes_per_iteration, width_height,
           learning_rate, num_epochs, num_input_time_steps, num_channels_one_hot_output, width_height_target, batch_size)
     pass
 
