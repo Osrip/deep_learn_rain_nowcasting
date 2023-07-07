Random Sampler implemented. Are weights really correct? Checked, that large weights indeed are assigned to pixels with much rain. However for whole frames the average weight of all pixels is taken. Observed in a small sample of frames that frames with high mean and max rain get assigned low avergae weights even though they should be large. May be coinicdence as there may be many outlier pixels in observed frames, have no other explanation at the moment. MAKE SURE THIS WORKS

load_data.py
@@ -238,6 +238,10 @@ def filtering_data_scraper(transform_f, last_input_rel_idx, target_rel_idx, s_fo
 
 def calc_class_frequencies(filtered_indecies, linspace_binning, mean_filtered_data, std_filtered_data, transform_f,
                            settings, s_num_bins_crossentropy, normalize=True, **__):
+    '''
+    The more often class occurs, the lower the weight value
+    TODO: However observed, that classes with lower mean and max precipitation have higher weight ??!!
+    '''
 
     class_count = torch.zeros(s_num_bins_crossentropy, dtype=torch.int64)
 
@@ -252,7 +256,12 @@ def calc_class_frequencies(filtered_indecies, linspace_binning, mean_filtered_da
 
     sample_num = torch.sum(class_count)
 
-    class_weights = sample_num / class_count
+    # class_weights = sample_num / class_count
+    class_weights = 1 / class_count
+
+    # TODO: How to handle class_count == 0 ? At the moment --> inf
+    # HHowever those classes that do not appear are never used, therefore it does not really matter what the
+    # Entries for those classes are.
     # Is this correct (from https://discuss.pytorch.org/t/how-to-handle-imbalanced-classes/11264/2 )
 
     return class_weights, class_count, sample_num
@@ -269,17 +278,13 @@ def class_weights_per_sample(filtered_indecies, class_weights, linspace_binning,
                                                                  transform_f,
                                                                  normalize=normalize, load_input_sequence=False,
                                                                  load_target=True, **settings)
-
+        # Checked this: Seems to do what it is supposed to (high rain (rare class) corresponds to high weight)
         target_one_hot_indecies = torch.argmax(target_one_hot, dim=0)
         target_class_weighted = class_weights[target_one_hot_indecies]
 
         mean_weight = torch.mean(target_class_weighted).item()
         target_mean_weights.append(mean_weight)
     return target_mean_weights
-
-
-
-    pass
 
 
 

train_lightning.py
@@ -6,7 +6,7 @@ from network_lightning import Network_l
 import datetime
 from load_data import PrecipitationFilteredDataset, filtering_data_scraper, lognormalize_data,\
     random_splitting_filtered_indecies, calc_class_frequencies, class_weights_per_sample
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 
 import numpy as np
 from helper.helper_functions import save_zipped_pickle, save_dict_pickle_csv,\
@@ -114,8 +114,11 @@ def data_loading(transform_f, settings, s_ratio_training_data, s_num_input_time_
                                                                     mean_filtered_data, std_filtered_data,
                                                                     transform_f, settings, normalize=True, **settings)
 
-    target_mean_weights = class_weights_per_sample(filtered_indecies, class_weights, linspace_binning, mean_filtered_data, std_filtered_data,
+
+    target_mean_weights = class_weights_per_sample(filtered_indecies_training, class_weights, linspace_binning, mean_filtered_data, std_filtered_data,
                                 transform_f, settings, normalize=True)
+
+
 
     # TODO: RETURN filtered indecies instead of data set
     train_data_set = PrecipitationFilteredDataset(filtered_indecies_training, mean_filtered_data, std_filtered_data,
@@ -127,7 +130,12 @@ def data_loading(transform_f, settings, s_ratio_training_data, s_num_input_time_
                                                        linspace_binning_min, linspace_binning_max, linspace_binning,
                                                        transform_f, **settings)
 
-    train_data_loader = DataLoader(train_data_set, batch_size=s_batch_size, shuffle=True, drop_last=True,
+    sampler = WeightedRandomSampler(weights=target_mean_weights, num_samples=len(train_data_set), replacement=True)
+    # TODO: Does this assume same order in weights as in data_set??
+    # replacement=True allows for oversampling and in exchange not showing all samples each epoch
+    # num_samples gives number of samples per epoch. Setting to len data_set forces sampler to not show all samples each epoch
+
+    train_data_loader = DataLoader(train_data_set, sampler=sampler, batch_size=s_batch_size, drop_last=True,
                                    num_workers=s_num_workers_data_loader)
 
 