b82cae3bc64de8ba4e2eef93b043f530ba77dad6
Jan <jan@u-086-c145.eap.uni-tuebingen.de>
2024-03-28 20:02:25 +0100

    minor changes

--- train_lightning.py
+++ train_lightning.py
@@ -356,7 +356,7 @@ if __name__ == '__main__':
     # train_start_date_time = datetime.datetime(2020, 12, 1)
     # s_folder_path = '/media/jan/54093204402DAFBA/Jan/Programming/Butz_AG/weather_data/dwd_datensatz_bits/rv_recalc/RV_RECALC/hdf/'
 
-    s_local_machine_mode = False
+    s_local_machine_mode = True
 
     s_force_data_preprocessing = True  # This forces data preprocessing instead of attempting to load preprocessed data
 
