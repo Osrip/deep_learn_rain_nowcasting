changed normalization back to wrong implementation as vorrect implementation with groupnorm or layernorm leads to no prediction

modules_blocks.py
@@ -111,18 +111,24 @@ class MetDilBlock(nn.Module):
 
         padding = 'same'
         self.dilation1 = nn.Conv2d(c_num, c_num, kernel_size, dilation=dilation, stride=stride, padding=padding)
-        self.group_norm1 = nn.GroupNorm(1, c_num)
+        self.layer_norm1_wrong = nn.LayerNorm(s_width_height)
+        # self.layer_norm1 = nn.LayerNorm([c_num, s_width_height, s_width_height])
+        # self.group_norm1 = nn.GroupNorm(1, c_num)
         self.dilation2 = nn.Conv2d(c_num, c_num, kernel_size, dilation=dilation, stride=stride, padding=padding)
-        self.group_norm2 = nn.GroupNorm(1, c_num)
+        self.layer_norm2_wrong = nn.LayerNorm(s_width_height)
+        # self.layer_norm2 = nn.LayerNorm([c_num, s_width_height, s_width_height])
+        # self.group_norm2 = nn.GroupNorm(1, c_num)
 
     def forward(self, x: torch.Tensor):
 
         out = x
         out = self.dilation1(out)
-        out = self.group_norm1(out)
+        out = self.layer_norm1_wrong(out)
+        # out = self.group_norm1(out)
         out = F.relu(out)
         out = self.dilation2(out)
-        out = self.group_norm2(out)
+        out = self.layer_norm2_wrong(out)
+        # out = self.group_norm2(out)
         out = F.relu(out)
 
         return x + out