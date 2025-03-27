ef68c96c5cbd194afc6464c3cb199f769e265e5d
Jan <jan.prosi@hotmail.com>
2025-03-27 16:28:52 +0100

    implemented configs as .yml, works locally

--- helper/settings_config_helper.py
+++ helper/settings_config_helper.py
@@ -13,12 +13,13 @@ def load_settings(mode):
     # In local mode, load the cluster_default then update with the local overrides if available.
     elif mode == 'local':
         settings = load_yaml_config('configs/cluster_default.yml')
-        if os.path.exists('configs/local_config.yml'):
-            local_overrides = load_yaml_config('configs/local_config.yml')
-            settings.update(local_overrides)
+        local_overrides = load_yaml_config('configs/local_config.yml')
+        settings.update(local_overrides)
     # In debug mode, use the debug_config file.
     elif mode == 'debug':
-        settings = load_yaml_config('configs/debug_config.yml')
+        settings = load_yaml_config('configs/cluster_default.yml')
+        debug_overrides = load_yaml_config('configs/debug_config.yml')
+        settings.update(debug_overrides)
     else:
         raise ValueError("Invalid mode. Choose from 'cluster', 'local', or 'debug'.")
     return settings
